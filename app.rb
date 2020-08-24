6fe38e566833f40dedcb1d7fa98b93bee3c73f6c
angela stephens <angelastephens317@gmail.com>
2020-08-23 23:58:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,22 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do 
+
+      erb :root
+    end
+
+    get '/new' do 
+      erb :'pirates/new'
+    end 
+
+    post '/pirates' do 
+      @pirate = Pirate.new(name:params[:pirate][:name], weight:params[:pirate][:weight], height:params[:pirate][:height])
+      @ships = Ship.new(params[:pirate][:ships][0])
+      @ship2 = Ship.new(params[:pirate][:ships][1])
+
+      erb :'pirates/show'
+    end
+
   end
 end
